b263282f71295c0d5cc9fa3edb706dcb2b29ec8e
Nicole Wu <wun@rpi.edu>
2024-11-09 22:24:15 -0500

    :pencil2:

--- app.js
+++ app.js
@@ -15,38 +15,11 @@ function initMap() {
         center: troyNY,
     });
 
-    // Add markers from Google My Maps link
-    fetch("https://www.google.com/maps/d/u/0/kml?mid=1WRdL1YfZysVpV1d3_A3q0uZE9enEFgE&output=kml")
-        .then(response => response.text())
-        .then(data => {
-            const parser = new DOMParser();
-            const kmlDoc = parser.parseFromString(data, "application/xml");
-            const placemarks = kmlDoc.querySelectorAll("Placemark");
-
-            placemarks.forEach(placemark => {
-                const name = placemark.querySelector("name").textContent;
-                const coords = placemark.querySelector("coordinates").textContent.trim().split(",");
-                const position = {
-                    lat: parseFloat(coords[1]),
-                    lng: parseFloat(coords[0]),
-                };
-
-                const marker = new google.maps.Marker({
-                    position,
-                    map,
-                    title: name,
-                });
-
-                const infoWindow = new google.maps.InfoWindow({
-                    content: `<h3>${name}</h3>`,
-                });
-
-                marker.addListener("click", () => {
-                    infoWindow.open(map, marker);
-                });
-            });
-        })
-        .catch(error => console.error("Error loading KML data:", error));
+    const kmlLayer = new google.maps.KmlLayer({
+        url: 'downtown.kml',
+        map: map,
+        preserveViewport: true,
+    });
 
     // Geolocation: Show the user's current location
     if (navigator.geolocation) {
